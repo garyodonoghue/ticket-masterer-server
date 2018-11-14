7628b06658af11f508f75b71c8dea8564fd4e7fb
Gary <gary.odonoghue@gmail.com>
2018-11-14 17:09:38 +0000

    Add get eventList and getEventDetails endpoints. Add CORS library

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express'); //npm install express
 var app = express();
+var cors = require('cors')
 require('dotenv').config()
+
+qpp.use(cors())
 
 //Load the request module
 var request = require('request'); //npm install request
@@ -8,7 +11,7 @@ var request = require('request'); //npm install request
 //Lets configure and request
 
 //accepts the redirect_uri in the form of http://localhost:8080/?code=blah
-app.get('/', function(req, res) {
+app.get('/oauth', function(req, res) {
     var auth_code = req.query.code;
 
     var success_body = "";
@@ -33,6 +36,47 @@ app.get('/', function(req, res) {
             console.log(success_body.access_token);
             console.log(success_body.refresh_token);
             res.redirect("http://localhost:4200/?access_token="+success_body.access_token+"&refresh_token"+success_body.refresh_token);
+        }
+    });
+});
+
+
+app.get('/listEvents', function(req, res) {
+
+    var keyword = req.query.keyword
+
+    var resp = request({
+        url: 'https://app.ticketmaster.com/discovery/v2/events.json?apikey='+process.env.CLIENT_ID+'&keyword='+keyword,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(response.statusCode);
+            res.json(response);
+        }
+    });
+});
+
+app.get('/getEventDetails', function(req, res) {
+
+    var eventId = req.query.events
+
+    var resp = request({
+        url: 'https://app.ticketmaster.com/inventory-status/v1/availability?apikey='+process.env.CLIENT_ID+'&events='+eventId,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(response.statusCode);
+            res.json(response);
         }
     });
 });
